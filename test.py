4c6477d4efc4c9bd9771c66c1702c2dbbff60f76
Maxence <max21maz07@gmail.com>
2019-04-11 22:00:25 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,24 +1,23 @@
 from v3 import *
+import pandas as pda
+import numpy as npy
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-    text = ("Liste des choix : \n - hash_tag_list : stream API : type (1)\n"
-    "- To select a twitter account and get information : type (2)\n"
-    "- To get tweets for a specific hashtag : type (3)\n"
-    "- If you want to do sentiment anaysis : type (4)\n"
-    "- To get the list of followers for a twitter account : type(5)\n"
-    "- To get specific infomations about a tweet : type (6)\n"
-    "- To plot a graph of likes or retweets through time for on account:"
-    " type(7)\n")
-
+    text = ("Liste des choix : \n - hash_tag_list : stream API : 1\n"
+    "- To select a twitter account and get information : 2\n"
+    "- To get tweets from user timeline: 3\n"
+    "- To get tweets with a specific #hashtag : 4\n"
+    "- To get the list of followers for a twitter account : 5\n")
     _continue = True
 
     while _continue is True:
         choice = int(input(text))
         # if not os.path.isfile(param): ValueError("need a python file")
         assert isinstance(choice, int), "%s is not a int" % choice
-        if choice not in [1,2,3,4,5,6, 7]:
-            print("choice has to be be between 1 & 7.")
+        if choice not in [1,2,3,4,5]:
+            print("choice has to be be between 1 & 5.")
         else:
             if choice == 1:
                 _input_list = input("Type in hashtag words: (ex: Donald Trump"
@@ -31,12 +30,12 @@ if __name__ == '__main__':
                 # ATTENTION : boucle infinie :#
                 twitter_streamer.stream_tweets(tweets_filename, hash_tag_list)
                 print("#===================== CONTINUE ?====================#")
-                answer = int(input(" Continue ? 0/1\n"))
-                if answer == 0:
+                answer = input(" Continue ? y/n\n")
+                if answer == "n":
                     _continue = False
             elif choice == 2:
                 _account = input("Type in twitter account\n(ex:realDonaldTrump"
-                "; EmmanuelMacron ; futuroscope ; SFR ; Apple ; univbordeaux)\n")
+                "; EmmanuelMacron ; futuroscope ; SFR ; elonmusk ; univbordeaux)\n")
                 twitter_client = TwitterClient(_account)
 
                 answer = input(" Get the first tweet of account timeline ? y/n\n")
@@ -55,70 +54,126 @@ if __name__ == '__main__':
                 if answer == "y":
                     print(twitter_client.get_home_timeline_tweets(1))
                 print("#===================== CONTINUE ?====================#")
-                answer = int(input(" Continue ? 0/1\n"))
-                if answer == 0:
+                answer = input(" Continue ? y/n\n")
+                if answer == "n":
                     _continue = False
             elif choice == 3:
                 _account = input("Type in twitter account\n(ex:realDonaldTrump"
-                "; EmmanuelMacron ; futuroscope ; SFR ; Apple ; univbordeaux)\n")
-
-                _hashtag = input(" Type in hashtag or key word wanted :\n"
-                "(ex: sfr ; sncf ; france)\n")
-
-                _lang = input("Which language ? (ex: en ; fr)\n")
-                _nb = int(input("How many tweets ?\n"))
-
+                "; EmmanuelMacron ; futuroscope ; SFR ; elonmusk ; univbordeaux)\n")
+                _nb = int(input("Number of tweets ?\n"))
                 _nb_df = int(input("How many tweets do you want to show"
-                "in the data frame ?\n"))
+                " in the data frame ?\n"))
 
                 twitter_client = TwitterClient(_account)
                 tweet_analyser = TweetAnalyser()
+                tweets = twitter_client.get_user_timeline_tweets(_nb)
+                df = tweet_analyser.tweets_to_data_frame(tweets)
 
-                _get_txt = ("- get_user_timeline_tweets : type(1)\n"
-                            "- get_friend_list : type(2)\n"
-                            "- get_home_timeline_tweets type(2)\n")
-                get_choice = int(input(_get_txt))
+                print(df.head(_nb_df))
+                print()
+                df_1 = pda.DataFrame(data=[_account], columns=['timeline Stats'])
+                df_1[' average len'] = npy.mean(df['len'])
+                df_1['+likes'] = npy.max(df['likes'])
+                df_1['+retweets'] = npy.max(df['retweets'])
+                print(df_1)
+                print()
+                _choice = input("-Plot nb of Likes through times : 1\n"
+                "-Plot nb of retweets through times : 2\n"
+                "-Plot both together : 3\n"
+                "-Pass : ' '\n")
+
+                if _choice == "1":
+                    time_likes = pda.Series(data=df['likes'].values,
+                                index=df['date'])
+                    time_likes.plot(figsize=(16, 4), color='r')
+                    plt.show()
+                elif _choice == "2":
+                    time_retweets = pda.Series(data=df['retweets'].values,
+                                index=df['date'])
+                    time_retweets.plot(figsize=(16, 4), color='r')
+                    plt.show()
+                elif _choice == "3":
+                    time_likes = pda.Series(data=df['likes'].values,
+                                index=df['date'])
+                    time_retweets = pda.Series(data=df['retweets'].values,
+                                index=df['date'])
+                    time_likes.plot(figsize=(16, 4), label="like", legend=True)
+                    time_retweets.plot(figsize=(16, 4), label="retweets",
+                                legend=True)
+                    plt.show()
+                print()
+                print("#===================== CONTINUE ?====================#")
+                answer = input(" Continue ? y/n\n")
+                if answer == "n":
+                    _continue = False
+            elif choice == 4:
+                _hashtag = input(" Type in hashtag or key word wanted :\n"
+                "(ex: sfr ; sncf ; france)\n")
+                _l = input("Which language ? (ex: en ; fr)\n")
+                _nb = int(input("Number of tweets ?\n"))
+                _nb_df = int(input("How many tweets do you want to show"
+                "in the data frame ?\n"))
+
+                twitter_client = TwitterClient()
+                tweet_analyser = TweetAnalyser()
 
                 # start_date = datetime.datetime(2019, 4, 8, 0, 0, 0)
                 # end_date = datetime.datetime(2018, 3, 28, 0, 0, 0)
                 # tweets = twitter_client.get_hashtag_tweets(20, 'sfr', 'fr', start_date)
 
-                if get_choice == 1:
-                    pass
-                elif get_choice == 2:
-                    pass
-                elif get_choice ==3:
-                    pass
-                tweets = twitter_client.get_hashtag_tweets(_nb, _hashtag, _lang)
+                tweets = twitter_client.get_hashtag_tweets(_nb, _hashtag, _l)
                 df = tweet_analyser.tweets_to_data_frame(tweets)
-                df['sentiment'] = npy.array([tweet_analyser.analyze_sentiment(tweet)for tweet in df['tweets']])
                 print(df.head(_nb_df))
                 print()
-                print("#===================== CONTINUE ?====================#")
-                answer = int(input(" Continue ? 0/1\n"))
-                if answer == 0:
-                    _continue = False
-            elif choice == 4:
-                print(" - Only available in English for now.")
+                df_1 = pda.DataFrame(data=[_hashtag], columns=[' # Stats'])
+                df_1[' average len'] = npy.mean(df['len'])
+                df_1['+likes'] = npy.max(df['likes'])
+                df_1['+retweets'] = npy.max(df['retweets'])
+                print(df_1)
+                print()
+                _choice = input("-Plot nb of Likes through times : 1\n"
+                "-Plot nb of retweets through times : 2\n"
+                "-Plot both together : 3\n"
+                "-Pass : ' '\n")
 
-                # df['sentiment'] = npy.array([tweet_analyser.analyze_sentiment(tweet)for tweet in df['tweets']])
-                # print(df.head(20))
+                if _choice == "1":
+                    time_likes = pda.Series(data=df['likes'].values,
+                                index=df['date'])
+                    time_likes.plot(figsize=(16, 4), color='r')
+                    plt.show()
+                elif _choice == "2":
+                    time_retweets = pda.Series(data=df['retweets'].values,
+                                index=df['date'])
+                    time_retweets.plot(figsize=(16, 4), color='r')
+                    plt.show()
+                elif _choice == "3":
+                    time_likes = pda.Series(data=df['likes'].values,
+                                index=df['date'])
+                    time_retweets = pda.Series(data=df['retweets'].values,
+                                index=df['date'])
+                    time_likes.plot(figsize=(16, 4), label="like", legend=True)
+                    time_retweets.plot(figsize=(16, 4), label="retweets",
+                                legend=True)
+                    plt.show()
+                print()
                 print("#===================== CONTINUE ?====================#")
-                answer = int(input(" Continue ? 0/1\n"))
-                if answer == 0:
+                answer = input(" Continue ? y/n\n")
+                if answer == "n":
                     _continue = False
             elif choice == 5:
+                _account = input("Type in twitter account\n(ex:realDonaldTrump"
+                "; EmmanuelMacron ; futuroscope ; SFR ; Apple ; univbordeaux)\n")
+                _nb = int(input("Number of followers ?\n"))
+                _nb_df = int(input("How many followers do you want to show"
+                "in the data frame ?\n"))
+
+                twitter_client = TwitterClient(_account)
+                follower_analyser = FollowerAnalyzer()
+                follower_list = twitter_client.get_follower_list(_nb)
+                df = follower_analyser.followers_to_data_frame(follower_list)
+                print(df.head(_nb_df))
+
                 print("#===================== CONTINUE ?====================#")
-                answer = int(input(" Continue ? 0/1\n"))
-                if answer == 0:
-                    _continue = False
-            elif choice == 6:
-                print("#===================== CONTINUE ?====================#")
-                answer = int(input(" Continue ? 0/1\n"))
-                if answer == 0:
-                    _continue = False
-            elif choice == 7:
-                print("#===================== CONTINUE ?====================#")
-                answer = int(input(" Continue ? 0/1\n"))
-                if answer == 0:
+                answer = input(" Continue ? y/n\n")
+                if answer == "n":
                     _continue = False
