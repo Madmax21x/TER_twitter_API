58887596abf192ee8045e0c359268af7876fb06f
Maxence <max21maz07@gmail.com>
2019-05-09 22:43:55 +0200

    Update v3_text_blob.py

--- v3_text_blob.py
+++ v3_text_blob.py
@@ -7,13 +7,12 @@ from tweepy import OAuthHandler
 from tweepy import Stream
 from textblob import TextBlob
 from textblob_fr import PatternTagger, PatternAnalyzer
-import datetime
 
 import ter_credentials as ter_c
 import numpy as npy
 import pandas as pda
 import re
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import json
 import collections
 
@@ -254,33 +253,5 @@ if __name__ == "__main__":
     twitter_client = TwitterClient()
     tweet_analyser = TweetAnalyser()
 
-    start_date = datetime.datetime(2019, 3, 28, 0, 0, 0)
-    # end_date = datetime.datetime(2018, 3, 28, 0, 0, 0)
-    date_since = "2019-04-11"
-    tweets = twitter_client.get_hashtag_tweets(20, 'boeing', 'en', date_since)
-
     # tweets = twitter_client.get_hashtag_tweets(_nb, _hashtag, _l)
-    df = tweet_analyser.tweets_to_data_frame(tweets)
-
-    positive = 0
-    negative = 0
-    neutral = 0
-    print(df['sentiment'].values)
-    for elem in df['sentiment'].values:
-        if elem > 0:
-            positive +=1
-        elif elem < 0:
-            negative +=1
-        else:
-            neutral +=1
-
-
-    colors = ['#00cec9', '#192a56', '#ffeaa7']
-    sizes = [positive, negative, neutral]
-    labels = 'Positive', 'Negative', 'Neutral'
-
-    plt.pie(x=sizes, shadow=True, colors=colors,labels=labels,startangle=90, autopct='%.1f%%')
-
-    #plt.title("Sentiment of {} Tweets about {}".format(number, query))
-    plt.show()
-    #print(df.head(10))
+    # df = tweet_analyser.tweets_to_data_frame(tweets)
